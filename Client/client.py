3623c3da46100d1f60d25f65c7412d6513a74949
ashunooji <ashuthoshnbgowda1102@gmail.com>
2024-04-17 10:58:38 +0530

    Changed the socket programming

--- Client/client.py
+++ Client/client.py
@@ -1,93 +1,30 @@
-import socket
 import os
+import utils
 import tenseal as ts
 from deepface import DeepFace
-import utils
-import pickle
 import shutil
+from tqdm import tqdm
 
+def compute(enc_imgs,context):
+    sample_image = 'input.jpg'
+    img_proto = DeepFace.represent(sample_image,model_name='Facenet')[0]['embedding']
+    img_ts = ts.plain_tensor(img_proto)
 
-def compute_distances():
-    sample_image_path = "input.jpg" #give path to your image
-
-    context = ts.context_from(utils.read_data('Recv_files\public.txt'))
-    image_proto = DeepFace.represent(sample_image_path,model_name='Facenet')[0]['embedding']
-    image_ts = ts.plain_tensor(image_proto)
-    for file in os.listdir('Recv_files'):
-        if file == "public.txt":
-            continue
-        enc_image_proto = utils.read_data(f'Recv_files\{file}')
-        enc_img = ts.lazy_ckks_vector_from(enc_image_proto)
-        enc_img.link_context(context)
-        euclidean_squard = image_ts - enc_img
+    for file in tqdm(os.listdir(enc_imgs)):
+        enc_img_proto = utils.read_data(f"{enc_imgs}/{file}")
+        enc_v = ts.lazy_ckks_vector_from(enc_img_proto)
+        enc_v.link_context(context)
+        euclidean_squard = img_ts - enc_v
         euclidean_squard = euclidean_squard.dot(euclidean_squard)
         utils.write_data(f"Result_files\{file[:-4]}.txt",euclidean_squard.serialize())
     print("Computation Done")
-    
 
-def receive_files(server_address, server_port, output_folder):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect((server_address, server_port))
 
-    while True:
-        filename = client_socket.recv(1024).decode()
-        if filename == "End":
-            break
-        if not filename:
-            break
-
-        client_socket.send(b'ACK')
-
-        with open(os.path.join(output_folder, filename), 'ab') as file:
-            file_data = client_socket.recv(1024)
-            while file_data and file_data != b'DONE':
-                file.write(file_data)
-                file_data = client_socket.recv(1024)
-
-        client_socket.send(b'ACK')
-
-    print("Files received successfully.")
-    print("Sending results..... ")
-    compute_distances()
-    print("Euclidean distance computed")
-
-    for filename in os.listdir('Result_files'):
-        file_path = os.path.join('Result_files', filename)
-        with open(file_path, 'rb') as file:
-            client_socket.send(filename.encode())
-            client_socket.recv(1024)  
-
-            file_data = file.read(1024)
-            while file_data:
-                client_socket.send(file_data)
-                file_data = file.read(1024)
-
-            client_socket.send(b'DONE')
-            client_socket.recv(1024)
-
-    client_socket.send("End".encode())
-    print("Encrypted results sent successfully")
-    sres = client_socket.recv(1024)
-    res = pickle.loads(sres)
-    if len(res) == 0:
-        print("No Faces detected in database")
-    else:
-        for i in res:
-            print(f"Faces detected : {i}")
-    client_socket.close()
-
-if __name__ == "__main__":
-    if os.path.exists("Recv_files"):
-        shutil.rmtree("Recv_files")
+if __name__  == "__main__":   
+    enc_imgs = r"D:\Major project\Virtual environment\Project\Edge\Encrypted_images"
+    context = ts.context_from(utils.read_data(r"D:\Major project\Virtual environment\Project\Edge\keys\public.txt"))
     if os.path.exists("Result_files"):
         shutil.rmtree("Result_files")
 
-    os.makedirs("Recv_files")
     os.makedirs("Result_files")
-    SERVER_ADDRESS = "localhost"
-    SERVER_PORT = 12345
-    OUTPUT_FOLDER = "Recv_files"  
-    receive_files(SERVER_ADDRESS, SERVER_PORT, OUTPUT_FOLDER)
-
-    
-
+    compute(enc_imgs,context)
--- Edge/encryption_prog_ser.py
+++ Edge/encryption_prog_ser.py
@@ -1,19 +1,13 @@
-import socket
-import cv2
-import numpy as np
 import os
 import shutil
 import utils
 from deepface import DeepFace
 import tenseal as ts
+from tqdm import tqdm
 
-host = '0.0.0.0'
-port = 5555
 image_folder = "rc"
 enc_img_folder = "Encrypted_images"
 
-if os.path.exists(image_folder):
-    shutil.rmtree(image_folder)
 if os.path.exists(enc_img_folder):
     shutil.rmtree(enc_img_folder)
 
@@ -21,37 +15,15 @@ os.makedirs(image_folder, exist_ok=True)
 os.makedirs(enc_img_folder,exist_ok=True)
 
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((host, port))
-server_socket.listen(1)  
-
-print(f"Server listening on {host}:{port}")
-
-client_socket, client_address = server_socket.accept()
-print(f"Connection from {client_address}")
-
-while True:
-    size = int.from_bytes(client_socket.recv(4), byteorder='big')
-    if size == 0:
-        continue
-    image_data = b""
-    while len(image_data) < size:
-        image_data += client_socket.recv(4096)
-
-    image_array = np.frombuffer(image_data, dtype=np.uint8)
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-
-    image_filename = f"rc_{len(os.listdir(image_folder)) + 1}.jpg"
-
-    cv2.imwrite(os.path.join(image_folder, image_filename), image)
-
-    print(f"Image saved: {image_filename}")
-
+for image_filename in tqdm(os.listdir("rc")):
     context = ts.context_from(utils.read_data('keys\secret.txt'))
-    image_embedding = DeepFace.represent(f'rc/{image_filename}',model_name='Facenet')[0]['embedding']
-    print(f"{image_filename} Embedding taken")
+    try:
+        image_embedding = DeepFace.represent(f'rc/{image_filename}',model_name='Facenet')[0]['embedding']
+    except ValueError:
+        continue
+    #print(f"{image_filename} Embedding taken")
     encrypted = ts.ckks_vector(context,image_embedding)
-    print(f"{image_filename} Encrypted")
+    #print(f"{image_filename} Encrypted")
     utils.write_data(f'Encrypted_images\{image_filename[:-4]}.txt',encrypted.serialize())
-    print("Encrypted file saved")
+    #print("Encrypted file saved")
 
